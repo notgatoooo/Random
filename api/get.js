b89a4738f8030dae8279d9c2059e4dfc7cbcb0bb
notgatoooo <peakgatoo@gmail.com>
2026-08-05 14:13:28 -0400

    Update get.js

--- api/get.js
+++ api/get.js
@@ -1,16 +1,35 @@
 export default function handler(req, res) {
-  const protocol = req.headers["x-forwarded-proto"] || "https";
-  const host = req.headers.host;
+  const headers = {};
 
+  const hidden = new Set([
+    "x-vercel-id",
+    "x-vercel-forwarded-for",
+    "x-vercel-proxy-signature",
+    "x-vercel-proxy-signature-ts",
+    "x-forwarded-for",
+    "x-forwarded-host",
+    "x-forwarded-port",
+    "x-forwarded-proto",
+    "x-real-ip",
+    "connection",
+  ]);
+
+  for (const [key, value] of Object.entries(req.headers)) {
+    if (!hidden.has(key.toLowerCase())) {
+      headers[key] = value;
+    }
+  }
+
+  const protocol = "https";
+  const host = req.headers.host;
   const url = new URL(req.url, `${protocol}://${host}`);
 
-  res.status(200).json({
+  res.json({
     args: req.query,
-    headers: req.headers,
+    headers,
     origin:
-      req.headers["x-forwarded-for"]?.split(",")[0] ||
-      req.socket?.remoteAddress ||
-      null,
+      req.headers["x-forwarded-for"]?.split(",")[0] ??
+      req.socket.remoteAddress,
     url: url.toString(),
   });
 }
