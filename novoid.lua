7026912a71f93d6c8806f17d02dbaa7319edce48
notgatoooo <peakgatoo@gmail.com>
2026-09-23 12:16:44 -0400

    Update novoid.lua

--- novoid.lua
+++ novoid.lua
@@ -5,6 +5,6 @@ local _3 = _.CreateTab("No Void", "/notgatoooo/Random/refs/heads/main/Assets/NoV
 local _4 = _3:AddSection("No Void", "MADE BY GATO 😎")
 
 _4:AddToggle("No Void", function(state)
-    workspace.FallenPartsDestroyHeight = state and (0 / 0) or original
+    workspace.FallenPartsDestroyHeight = state and (0 / 0) or _2
     -- print("ok", state)
 end)
